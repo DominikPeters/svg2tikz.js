51fd0fd1d6ca448c20568567d99acc2591950221
Dominik Peters <3543224+DominikPeters@users.noreply.github.com>
2026-03-06 21:26:25 +0100

    don't commit dist/

--- dist/src/svg2tikz.d.ts
+++
@@ -1,6 +0,0 @@
-export interface SvgToTikzOptions {
-    precision?: number;
-    scale?: number | null;
-    standalone?: boolean;
-}
-export declare function svgToTikz(svgInput: string | Element, options?: SvgToTikzOptions): string;
